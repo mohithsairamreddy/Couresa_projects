fdb21a4de72cfe75902a9def1652322edc257443
mohithsairamreddy <mohithsairamreddy@users.noreply.github.com>
2021-03-24 09:50:28 +0530

    Project

--- Assignment 4/script.js
+++ Assignment 4/script.js
@@ -4,19 +4,19 @@ Solution of assignment 4:
 Expected output:
 
 Hello Yaakov
-Good Bye Mohith
-Good Bye Latha
-Good Bye Dayakar
-Hello Dad
-Hello Mom
-Hello Brother
-Hello Bhaswanth
-Hello Amrita
-Good Bye India
+Good Bye John
+Good Bye Jen
+Good Bye Jason
+Hello Paul
+Hello Frank
+Hello Larry
+Hello Paula
+Hello Laura
+Good Bye Jim
 */
 
 (function() {
-    var names = ["Yaakov", "Mohith", "Latha", "Dayakar", "Dad", "Mom", "Brother", "Bhaswanth", "Amrita", "India"];
+    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
     for (var i = 0; i < names.length; i++) {
         var firstLetter = names[i].charAt(0).toLowerCase();
         if (firstLetter === 'j') {
